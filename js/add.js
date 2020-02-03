c40d31db35a0f2b0f763fa0fdd5af520e4f3e9ca
Onur Şerbetçi <data-science-home@users.noreply.github.com>
2020-02-03 04:06:36 +0300

    Update add.js

--- js/add.js
+++ js/add.js
@@ -24,7 +24,7 @@ add.onclick= function() {
 
     xmlhttp.onreadystatechange = function() {
         var myArr = JSON.parse(this.responseText);
-        document.getElementById("rows").innerHTML = this.rows.value
+        document.getElementById("rows").innerHTML = myArr.rows
     };
 };
 
