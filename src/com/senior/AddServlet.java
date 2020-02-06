9a45c806e451856374d4ea98315dd12c57b3e075
Onur Şerbetçi <data-science-home@users.noreply.github.com>
2020-02-06 15:51:50 +0300

    version 0.13 (correction)

--- src/com/senior/AddServlet.java
+++ src/com/senior/AddServlet.java
@@ -32,119 +32,142 @@ public class AddServlet extends HttpServlet
 
 	public void service(HttpServletRequest req,HttpServletResponse res) 
 	{	
-		try {
 		String api_key = req.getParameter("api_key"); // Integer.parseInt(req.getPara..)
 		String adress = req.getParameter("adress");
+		String mode = req.getParameter("mode");
+		String unit = req.getParameter("unit");
 		
-		PrintWriter out  = res.getWriter();
-		
-		 String origins = adress;
-		 origins = origins.replace(" ", "%");
-		 origins = origins.replace(",", "|");
-		 
-		 //System.out.println("Please write your destinations: \n ");
-		 //String destinations=ob.readLine();
-		 String destinations = origins;
-		 
-		 String format="xml";
-		 String mode = "driving";
-		 String language= "tr-TR";
-		 String url = "https://maps.googleapis.com/maps/api/distancematrix/"+format+"?origins="+origins+"&destinations="+destinations+
-				 "&mode="+mode+"&language="+language+"&key="+api_key;
-		 out.println(url);
-		 URL obj = new URL(url);
-		 HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-		 int responseCode = con.getResponseCode();
-		 out.println("Response Code : " + responseCode);
-		  BufferedReader in = new BufferedReader(
-			 new InputStreamReader(con.getInputStream()));
-			 String inputLine;
-			 StringBuffer response = new StringBuffer();
-			 while ((inputLine = in.readLine()) != null) {
-			   response.append(inputLine);
-			 } 
-			in.close();
-			//print in String
-			out.println(response.toString());
-		        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-		         .parse(new InputSource(new StringReader(response.toString())));
-			doc.getDocumentElement().normalize();
-           out.println ("Root element of the doc is " +
-                   doc.getDocumentElement().getNodeName());
-           NodeList listofElements = doc.getElementsByTagName("element"); 
-           NodeList listofDuration = doc.getElementsByTagName("duration"); 
-           NodeList listofDistance = doc.getElementsByTagName("distance"); 
-		   
-           ArrayList<Integer> durationMatrixAll = new ArrayList<>();
-           ArrayList<Integer> distanceMatrixAll = new ArrayList<>();
-           int totalElements = listofElements.getLength(); 
-           out.println("Total number of elements : " + (int) Math.sqrt(totalElements)+"x"+(int)Math.sqrt(totalElements));
-           for (int s=0; s<listofElements.getLength() ; s++) {
 
-                   Node firstElNode = listofElements.item(s);
-                   if(firstElNode.getNodeType() == Node.ELEMENT_NODE) {
-                   	Node firstDurNode = listofDuration.item(s);
-                   	if(firstDurNode.getNodeType() == Node.ELEMENT_NODE) {
-                   		Element firstElement = (Element)firstDurNode;
-                   		NodeList durationList = firstElement.getElementsByTagName("value"); 
-                   		Element durationElement = (Element)durationList.item(0);
-                   		NodeList textDurationList = durationElement.getChildNodes();
-                   		//System.out.println("duration "+s+": " + ((Node)textDurationList.item(0)).getNodeValue().trim() + " sec");
-                   		int result = Integer.parseInt(((Node)textDurationList.item(0)).getNodeValue().trim());
-                   		durationMatrixAll.add(result);
-                   	}
-                   	Node firstDistNode = listofDistance.item(s);
-                   	if(firstDistNode.getNodeType() == Node.ELEMENT_NODE) {
-                   		Element firstElement = (Element)firstDistNode;
-                   		NodeList distanceList = firstElement.getElementsByTagName("value");
-                   		Element distanceElement = (Element)distanceList.item(0);
-                   		NodeList textDistanceList = distanceElement.getChildNodes();
-                   		//System.out.println("distance "+s+": " + ((Node)textDistanceList.item(0)).getNodeValue().trim() + " m");
-                       	int result = Integer.valueOf(((Node)textDistanceList.item(0)).getNodeValue().trim());
-                       	distanceMatrixAll.add(result);
-                   }
-               } 
-           }
-           out.println("\n- - - - - Distance Matrix - - - - -");
-           int size = (int) Math.sqrt(totalElements);
-           int[][] distanceMatrix = new int[size][size];
-           distanceMatrix=to2dArray(distanceMatrixAll);
-           for(int[] row : distanceMatrix) {
-               printRow(row,res);
-           }
-           out.println("\n- - - - - Duration Matrix - - - - -");
-           int[][] durationMatrix = new int[size][size];
-           durationMatrix=to2dArray(durationMatrixAll);
-           for(int[] row : durationMatrix) {
-               printRow(row,res);
-           }}
-           catch (SAXParseException err) {
-        	   PrintWriter out;
-			try {
-				out = res.getWriter();
-	            out.println("** Parsing error" + ", line " + err.getLineNumber() + ", uri " + err.getSystemId());
-	            out.println(" " + err.getMessage ());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-           }catch (SAXException e) {
-               Exception x = e.getException ();
-               ((x == null)? e :x).printStackTrace();
-           }catch (Throwable t) {
-               t.printStackTrace ();
-           }
+		
+		findMatrices(api_key,adress,res,mode,unit);
+		
+
        	
 		}
-       	private static int[][] to2dArray(ArrayList<Integer> arrayList) {
-               ArrayList<Integer> temp =new ArrayList<>();
-               int size = (int) Math.sqrt(arrayList.size());
-               int[][] array = new int[size][size];
+	
+	
+     private void findMatrices(String api_key, String adress, HttpServletResponse res, String mode, String unit) {
+       	try {
+       	PrintWriter out  = res.getWriter();
+    		
+   		 String origins = adress;
+   		 origins = origins.replace(" ", "%");
+   		 origins = origins.replace(",", "|");
+   		 
+   		 String destinations = origins;
+   		 
+   		 String format="xml";
+   		 String language= "tr-TR";
+   		 String url = "https://maps.googleapis.com/maps/api/distancematrix/"+format+"?origins="+origins+"&destinations="+destinations+
+   				 "&mode="+mode+"&language="+language+"&key="+api_key;
+   		 out.println(url);
+   		 URL obj = new URL(url);
+   		 HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+   		 int responseCode = con.getResponseCode();
+   		 out.println("Response Code : " + responseCode);
+   		  BufferedReader in = new BufferedReader(
+   			 new InputStreamReader(con.getInputStream()));
+   			 String inputLine;
+   			 StringBuffer response = new StringBuffer();
+   			 while ((inputLine = in.readLine()) != null) {
+   			   response.append(inputLine);
+   			 } 
+   			in.close();
+   			//print in String
+   			out.println(response.toString());
+   		        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+   		         .parse(new InputSource(new StringReader(response.toString())));
+   			doc.getDocumentElement().normalize();
+              out.println ("Root element of the doc is " +
+                      doc.getDocumentElement().getNodeName());
+              NodeList listofElements = doc.getElementsByTagName("element"); 
+              NodeList listofDuration = doc.getElementsByTagName("duration"); 
+              NodeList listofDistance = doc.getElementsByTagName("distance"); 
+   		   
+              ArrayList<Float> durationMatrixAll = new ArrayList<>();
+              ArrayList<Float> distanceMatrixAll = new ArrayList<>();
+              int totalElements = listofElements.getLength(); 
+              out.println("Total number of elements : " + (int) Math.sqrt(totalElements)+"x"+(int)Math.sqrt(totalElements));
+              for (int s=0; s<listofElements.getLength() ; s++) {
+
+                      Node firstElNode = listofElements.item(s);
+                      if(firstElNode.getNodeType() == Node.ELEMENT_NODE) {
+                      	Node firstDurNode = listofDuration.item(s);
+                      	if(firstDurNode.getNodeType() == Node.ELEMENT_NODE) {
+                      		Element firstElement = (Element)firstDurNode;
+                      		NodeList durationList = firstElement.getElementsByTagName("value"); 
+                      		Element durationElement = (Element)durationList.item(0);
+                      		NodeList textDurationList = durationElement.getChildNodes();
+                      		float result=0;
+                      		if(unit != "meter") {
+                          		result = Integer.parseInt(((Node)textDurationList.item(0)).getNodeValue().trim());
+                      			result = result/60;
+                          		durationMatrixAll.add(result);
+                      		} else {
+                      			result = (int) result;
+                              	result = Integer.valueOf(((Node)textDurationList.item(0)).getNodeValue().trim());
+                              	distanceMatrixAll.add(result);
+                      		}
+                      	}
+                      	Node firstDistNode = listofDistance.item(s);
+                      	if(firstDistNode.getNodeType() == Node.ELEMENT_NODE) {
+                      		Element firstElement = (Element)firstDistNode;
+                      		NodeList distanceList = firstElement.getElementsByTagName("value");
+                      		Element distanceElement = (Element)distanceList.item(0);
+                      		NodeList textDistanceList = distanceElement.getChildNodes();
+                      		float result=0;
+                      		if(unit != "meter") {
+                              	result = Integer.valueOf(((Node)textDistanceList.item(0)).getNodeValue().trim());
+                      			result = (float) result/1000;
+                              	distanceMatrixAll.add(result);
+                      		} else {
+                      			result = (int) result;
+                              	result = Integer.valueOf(((Node)textDistanceList.item(0)).getNodeValue().trim());
+                              	distanceMatrixAll.add(result);
+                      		}
+                      }
+                  } 
+              }
+              out.println("\n- - - - - Distance Matrix - - - - -");
+              int size = (int) Math.sqrt(totalElements);
+              float[][] distanceMatrix = new float[size][size];
+              distanceMatrix=to2dArray(distanceMatrixAll);
+              for(float[] row : distanceMatrix) {
+                  printRow(row,res);
+              }
+              out.println("\n- - - - - Duration Matrix - - - - -");
+              float[][] durationMatrix = new float[size][size];
+              durationMatrix=to2dArray(durationMatrixAll);
+              for(float[] row : durationMatrix) {
+                  printRow(row,res);
+              }}
+              catch (SAXParseException err) {
+           	   PrintWriter out;
+   			try {
+   				out = res.getWriter();
+   	            out.println("** Parsing error" + ", line " + err.getLineNumber() + ", uri " + err.getSystemId());
+   	            out.println(" " + err.getMessage ());
+   			} catch (IOException e) {
+   				// TODO Auto-generated catch block
+   				e.printStackTrace();
+   			}
+              }catch (SAXException e) {
+                  Exception x = e.getException ();
+                  ((x == null)? e :x).printStackTrace();
+              }catch (Throwable t) {
+                  t.printStackTrace ();
+              }
+		
+	}
+		private static float[][] to2dArray(ArrayList<Float> durationMatrixAll) {
+               ArrayList<Float> temp =new ArrayList<>();
+               int size = (int) Math.sqrt(durationMatrixAll.size());
+               float[][] array = new float[size][size];
                int index = 0;
        		for (int i = 0; i < size; i++) {
        			for (int j = 0; j < size; j++){
        				index = (size)*i+j;
-       				temp.add(arrayList.get(index));
+       				temp.add(durationMatrixAll.get(index));
        			}
        			//System.out.println(temp.toString());
        			for (int x = 0; x < size; x++){
@@ -154,13 +177,13 @@ public class AddServlet extends HttpServlet
        		return array;
        	}
        	
-           public static void printRow(int[] row, HttpServletResponse res) throws IOException {
+           public static void printRow(float[] row, HttpServletResponse res) throws IOException {
         	   PrintWriter out  = res.getWriter();
-               for (int i : row) {
+               for (float i : row) {
                    out.print(i);
                    out.print("\t");
                }
-               out.println("\n");
+               out.print("\n");
            }
 			
 }
